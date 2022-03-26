82f9c1056fa612d696859f3e4f0e9d3da4513921
GunayM <gunay.nur1303@gmail.com>
2022-03-26 19:37:37 -0400

    I have just added WebElement locators TryCloudLogin class

--- src/test/java/com/trycloud/pages/TryCloudLogin.java
+++ src/test/java/com/trycloud/pages/TryCloudLogin.java
@@ -1,5 +1,27 @@
 package com.trycloud.pages;
 
-public class TryCloudLogin {
+import org.openqa.selenium.WebElement;
+import org.openqa.selenium.support.FindBy;
+
+public class TryCloudLogin extends BasePage {
+
+
+    @FindBy(id = "user")
+    public WebElement userInput;
+
+    @FindBy(id = "password")
+    public WebElement passwordInput;
+
+    @FindBy(id = "submit-form")
+    public WebElement submitFormInput;
+
+    @FindBy(id = "lost-password")
+    public WebElement lostpasswordLink;
+
+    @FindBy(xpath = "//a[normalize-space(text()) = 'Log in with a device']")
+    public WebElement elementLink;
+
+    @FindBy(css = ".info")
+    public WebElement infoP;
 
 }
