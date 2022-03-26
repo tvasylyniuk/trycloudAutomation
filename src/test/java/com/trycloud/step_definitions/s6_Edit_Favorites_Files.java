5ee14a9c73cfb0ef7cdf1617ccb8a875c0246804
oz_ah <oz_ahmed@hotmail.com>
2022-03-25 20:32:17 -0400

    UserStory 6

--- src/test/java/com/trycloud/step_definitions/s6_Edit_Favorites_Files.java
+++ src/test/java/com/trycloud/step_definitions/s6_Edit_Favorites_Files.java
@@ -3,24 +3,31 @@ package com.trycloud.step_definitions;
 import io.cucumber.java.en.*;
 
 public class s6_Edit_Favorites_Files {
-    @Given("user on the dashboard page")
-    public void user_on_the_dashboard_page() {
-
-    }
     @When("the user clicks the {string} module")
     public void the_user_clicks_the_module(String string) {
 
-    }
-    @When("the user clicks the add icon on the top")
-    public void the_user_clicks_the_add_icon_on_the_top() {
+
+
 
     }
-    @When("user uploads file with the upload file option")
-    public void user_uploads_file_with_the_upload_file_option() {
-
+    @When("the users click action-icon from any file on the page to remove")
+    public void the_users_click_action_icon_from_any_file_on_the_page_to_remove() {
+        // Write code here that turns the phrase above into concrete actions
+        throw new io.cucumber.java.PendingException();
     }
-    @Then("Verify the file is displayed on the page")
-    public void verify_the_file_is_displayed_on_the_page() {
-
+    @When("user choose the {string} option")
+    public void user_choose_the_option(String string) {
+        // Write code here that turns the phrase above into concrete actions
+        throw new io.cucumber.java.PendingException();
+    }
+    @When("user click the {string} sub-module on the left side")
+    public void user_click_the_sub_module_on_the_left_side(String string) {
+        // Write code here that turns the phrase above into concrete actions
+        throw new io.cucumber.java.PendingException();
+    }
+    @Then("Verify that the file is removed from the Favorites sub-module’s table")
+    public void verify_that_the_file_is_removed_from_the_favorites_sub_module_s_table() {
+        // Write code here that turns the phrase above into concrete actions
+        throw new io.cucumber.java.PendingException();
     }
 }
