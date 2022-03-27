5be387a47dd4179d234bfc7c49da7815de9aacef
Tariq Ghafoori <tghafoori3@gmail.com>
2022-03-26 22:23:56 -0400

    Creation of my Step Defs US-4 - Tariq

---
+++ src/test/java/com/trycloud/step_definitions/s4_FilesModule_StepDefs.java
@@ -0,0 +1,53 @@
+package com.trycloud.step_definitions;
+
+import com.trycloud.pages.FilesPage;
+import com.trycloud.pages.TryCloudLogin;
+import com.trycloud.utilities.BrowserUtils;
+import com.trycloud.utilities.ConfigurationReader;
+import com.trycloud.utilities.Driver;
+import io.cucumber.java.en.And;
+import io.cucumber.java.en.Given;
+import io.cucumber.java.en.Then;
+import io.cucumber.java.en.When;
+import org.junit.Assert;
+import org.openqa.selenium.support.ui.Select;
+
+public class s4_FilesModule_StepDefs {
+
+    //Scenario #1
+
+    FilesPage filesPage = new FilesPage();
+
+    @When("the user clicks {string} module")
+    public void theUserClicksModule(String files) {
+        filesPage.filesTab.click();
+
+    }
+
+    @Then("verify the page title is \"Files - Trycloud QA.”")
+    public void verify_the_page_title_is_files_trycloud_qa() {
+        String expectedTitle = "Files - Trycloud QA";
+        String actualTitle = Driver.getDriver().getTitle();
+        Assert.assertTrue(actualTitle.contains(expectedTitle));
+    }
+
+
+    //Scenario #2
+
+
+
+    @And("user click the top-left checkbox of the table")
+    public void userClickTheTopLeftCheckboxOfTheTable() {
+        filesPage.filesTab.click();
+        filesPage.leftCheckbox.click();
+
+    }
+
+    @Then("verify all the files are selected")
+    public void verifyAllTheFilesAreSelected() {
+        Assert.assertTrue(filesPage.checkBoxes.isSelected());
+    }
+
+
+}
+
