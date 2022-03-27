db775ec747b3899315a566ebb29139accd9071bf
Regina <musovirova@yahoo.com>
2022-03-27 00:52:06 -0400

    fix features

--- src/test/java/com/trycloud/runner/FailedTestsRunner.java
+++ src/test/java/com/trycloud/runner/FailedTestsRunner.java
@@ -6,7 +6,7 @@ import org.junit.runner.RunWith;
 
 @RunWith(Cucumber.class)
 @CucumberOptions(
-        features = "",
+        features = "@target/rerun.txt",
         glue = "com/trycloud/step_definitions"
 
 
