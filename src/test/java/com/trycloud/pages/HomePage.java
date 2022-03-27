f3acf8c27e86d64e05f0867cad59ef7d3f8eec7b
oz_ah <oz_ahmed@hotmail.com>
2022-03-27 01:39:24 -0400

    HomePage

--- src/test/java/com/trycloud/pages/HomePage.java
+++ src/test/java/com/trycloud/pages/HomePage.java
@@ -3,7 +3,7 @@ package com.trycloud.pages;
 import org.openqa.selenium.WebElement;
 import org.openqa.selenium.support.FindBy;
 
-public class HomePage extends BasePage {
+public class HomePage extends BasePage{
 
 
 
